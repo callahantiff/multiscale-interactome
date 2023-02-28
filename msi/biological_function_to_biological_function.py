5d4694ce10fc3d21b9219235618fa555923ef2cd
callahantiff <callahantiff@gmail.com>
2023-02-28 13:24:28 -0500

    adding shebang lines and docstrings

--- msi/biological_function_to_biological_function.py
+++ msi/biological_function_to_biological_function.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python 3
+# -*- coding: utf-8 -*-
+
+# import needed libraries
 from msi.node_to_node import NodeToNode
 
 
 class BiologicalFunctionToBiologicalFunction(NodeToNode):
-	def __init__(self, directed, file_path, sep="\t"):
+	"""
+	Class inherits functionality from the NodeToNode Class.
+	"""
+	def __init__(self, directed, file_path, sep="\t") -> None:
 		super().__init__(directed, file_path, sep)
--- msi/protein_to_biological_function.py
+++ msi/protein_to_biological_function.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python 3
+# -*- coding: utf-8 -*-
+
+# import needed libraries
 from msi.node_to_node import NodeToNode
 
 
 class ProteinToBiologicalFunction(NodeToNode):
-	def __init__(self, file_path, sep="\t"):
+	"""
+	Class inherits functionality from the NodeToNode Class.
+	"""
+	def __init__(self, file_path, sep="\t") -> None:
 		super().__init__(file_path, sep)
