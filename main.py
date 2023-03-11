81c2bfeaf1a4e484cc5e8662f377c9e6a0ae36a8
callahantiff <callahantiff@gmail.com>
2023-03-10 22:38:56 -0500

    tidying

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from diff_prof.diffusion_profiles import *
 
 ### STEP 1 -- Build Multiscale Interactome
 # set paths to data files
-core_dir = 'resources/data/'
-drug2protein_file_path = core_dir + '1_drug_to_protein.tsv'
-indication2protein_file_path = core_dir + '2_indication_to_protein.tsv'
-protein2protein_file_path = core_dir + '3_protein_to_protein.tsv'
-protein2biological_function_file_path = core_dir + '4_protein_to_biological_function.tsv'
-biological_function2biological_function_file_path = core_dir + '5_biological_function_to_biological_function.tsv'
+core_dir = "resources/data/"
+drug2protein_file_path = core_dir + "1_drug_to_protein.tsv"
+indication2protein_file_path = core_dir + "2_indication_to_protein.tsv"
+protein2protein_file_path = core_dir + "3_protein_to_protein.tsv"
+protein2biological_function_file_path = core_dir + "4_protein_to_biological_function.tsv"
+biological_function2biological_function_file_path = core_dir + "5_biological_function_to_biological_function.tsv"
 
 # construct the multiscale interactome
 msi = MSI(drug2protein_directed=False, indication2protein_directed=False, protein2protein_directed=False,
@@ -23,27 +23,27 @@ msi = MSI(drug2protein_directed=False, indication2protein_directed=False, protei
           biological_function2biological_function_file=biological_function2biological_function_file_path)
 msi.load()
 
-# save network and node information dict (also gets saved when building diffusion profiles)
-msi.save_graph('resources/data/mis_graph_v1.pkl')
-msi.save_node2idx('resources/data/mis_graph_v1_node2idx.pkl')
+# # save network and node information dict (also gets saved when building diffusion profiles)
+# msi.save_graph("results/msi_graph.pkl")
+# msi.save_node2idx("results/msi_graph_node2idx.pkl")
 
 
 ### STEP 2 -- Build Diffusion Profiles
 # set inputs with default values according to README instructions
 alpha = 0.8595436247434408
 weight_dict = {
-    'down_biological_function':  4.4863053901688685,
-    'indication': 3.541889556309463,
-    'biological_function': 6.583155399238509,
-    'up_biological_function': 2.09685000906964,
-    'protein': 4.396695660380823,
-    'drug': 3.2071696595616364
+    "down_biological_function":  4.4863053901688685,
+    "indication": 3.541889556309463,
+    "biological_function": 6.583155399238509,
+    "up_biological_function": 2.09685000906964,
+    "protein": 4.396695660380823,
+    "drug": 3.2071696595616364
 }
 
 # derive diffusion profiles
+cores = int(multiprocessing.cpu_count()/2) - 4 if int(multiprocessing.cpu_count()/2) - 4 > 0 else 1
 dp = DiffusionProfiles(alpha=alpha, max_iter=1000, tol=1e-06, weights=weight_dict,
-                       num_cores=int(multiprocessing.cpu_count()/2) - 4,
-                       save_load_file_path="results/")
+                       num_cores=cores, save_load_file_path="results/")
 dp.calculate_diffusion_profiles(msi)
 
 # load a saved diffusion profile
